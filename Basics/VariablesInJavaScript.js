66530b66e4db9ddc174287653fe4fcc041290f5b
PranayDange <pranaydange04@gmail.com>
2023-05-07 13:01:27 +0530

    var let and const in javascript

--- Basics/VariablesInJavaScript.js
+++ Basics/VariablesInJavaScript.js
@@ -1,6 +1,37 @@
-var a = 64;
+console.log("var let and const")
+
+
+//const --constant
+//var/let --it is used to create a variable
+
+//var was used before es6
+
+
+// var a = 64;
+// console.log(a)
+
+
+// var a = 76;
+// console.log(a)
+
+
+//In javascript you can change variable type at runtime 
+//these are known dynamically typed languages
+//eg : python ,javascript
+//static tyepd languages ex: c++,java,swift
+
+// var a = 67;
+// console.log(a)
+
+let a = 45;
+console.log(a)
+
+a="pranay"
+console.log(a)
+
 console.log(a)
 
 
-var a = 76;
-console.log(a)
+
+// it is generally recommended to use let instead of var 
+// when declaring variables in modern JavaScript code.
