881b9c11f1b0bf093b02d954ccb7357ec206a672
PranayDange <pranaydange04@gmail.com>
2023-05-09 07:13:39 +0530

    operators in javascript

---
+++ Basics/ConditionalExpressions.js
@@ -0,0 +1,6 @@
+let a = prompt("hey what is your age?");
+a = Number.parseInt(a);
+console.log(a);
+
+
+alert("this is not valid age")
---
+++ Basics/Operaters.js
@@ -0,0 +1,18 @@
+console.log("operaters in js");
+
+let a = 64;
+let b = 4;
+
+console.log("a **b = ", a ** b); //this will used as a power of 64 power of 4 =1,67,77,216
+
+//==   --equal to
+//!=   --not equal to
+//===  -- equal value and type
+
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2", comp1 == comp2);
+console.log("comp1 == comp2", comp1 != comp2);
+console.log("comp1 === comp2", comp1 === comp2);
+console.log("comp1 !== comp2", comp1 !== comp2);
+
